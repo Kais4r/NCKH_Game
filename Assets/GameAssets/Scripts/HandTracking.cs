810c4b95740d4b505fb9442c7681cd2a5acddb31
Kais4r <143178887+Kais4r@users.noreply.github.com>
2024-04-15 12:13:18 +0700

    Update camera and test cube position

--- Assets/GameAssets/Scripts/HandTracking.cs
+++ Assets/GameAssets/Scripts/HandTracking.cs
@@ -19,7 +19,7 @@ public class HandTracking : MonoBehaviour
 
         for(int i = 0; i < 21; i++)
         {
-            float x = 7 - float.Parse(points[i * 3])/100;
+            float x = float.Parse(points[i * 3])/100 - 7;
             float y = float.Parse(points[i * 3 + 1]) / 100;
             float z = float.Parse(points[i * 3 + 2]) / 100;
 
