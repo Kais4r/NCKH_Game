fe159648add865ec13aeb0d0a967c5e02e76e88e
Kais4r <143178887+Kais4r@users.noreply.github.com>
2024-04-23 23:45:15 +0700

    Update level select

---
+++ Assets/GameAssets/Scenes/LevelSelection/Scripts/SelectLevel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectLevel : MonoBehaviour
+{
+    /*[SerializeField] private GameObject _panel;
+    private GameManagerSingleton _gameManagerSingleton;
+    private void Awake()
+    {
+        _gameManagerSingleton = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerSingleton>();
+    }
+    public void TurnOnOrOffPanel(string level)
+    {
+        _gameManagerSingleton.cefrLevel = level;
+        _panel.SetActive(true);
+
+    }*/
+}
