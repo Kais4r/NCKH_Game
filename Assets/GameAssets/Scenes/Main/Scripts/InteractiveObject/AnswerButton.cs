96ee85a94e67486e72ed876677fa95d9d9f6c3ce
GiaBaoK46DLU <dlgb260704@gmail.com>
2024-04-24 07:47:07 +0700

    Pressing button

--- Assets/GameAssets/Scenes/Main/Scripts/InteractiveObject/AnswerButton.cs
+++ Assets/GameAssets/Scenes/Main/Scripts/InteractiveObject/AnswerButton.cs
@@ -1,27 +1,47 @@
+using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnswerButton : MonoBehaviour
 {
     [SerializeField] private MainGameManager _mainGameManager;
     [SerializeField] private TextMeshProUGUI _buttonAnswerText;
+    Vector3 initialPos;
+    Rigidbody rb;
+  
+
     public List<Material> danhSachMau;
+    private void Start()
+    {
+        initialPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+
+
     void OnTriggerEnter(Collider other)
     {
+
         if (other.gameObject.CompareTag("PlayerHand"))
         {
+         
+
             if (_mainGameManager.gameState == GameState.ProcessAnswer)
             {
-
+                
+                    rb.AddForce(transform.forward * -4f);
+                    StartCoroutine(enumerator(0.5f));
             }
-            else
+            else 
             {
                 _mainGameManager.ChooseAnswer(_buttonAnswerText.text);
                 if (_mainGameManager.answerResult == true)
                 {
-                    //Doi mau xanh
-                    //StartCourutine
+                   
                 }
                 else
                 {
@@ -33,5 +53,18 @@ public class AnswerButton : MonoBehaviour
         // doi mau xanh, do
         // Yield wait return 1f;
         // tra ve mau trang
+        IEnumerator enumerator(float t)
+        {
+            yield return new WaitForSeconds(t);
+            rb.velocity = Vector3.zero;
+            ResetPos();
+        }
     }
+    private void ResetPos()
+    {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        transform.position = initialPos;
+    }
+
 }
